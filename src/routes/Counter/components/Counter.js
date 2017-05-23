e85e5711adab39e4fc88b44e216da2fe27854a7e
xuhang <xuhang@gomeholdings.com>
2017-05-23 11:13:40 +0800

    调整indent

--- src/routes/Counter/components/Counter.js
+++ src/routes/Counter/components/Counter.js
@@ -2,21 +2,21 @@ import React from 'react'
 import PropTypes from 'prop-types'
 
 export const Counter = (props) => (
-  <div style={{ margin: '0 auto' }} >
-    <h2>Counter: {props.counter}</h2>
-    <button className='btn btn-primary' onClick={props.increment}>
-      Increment
+    <div style={{ margin: '0 auto' }} >
+        <h2>Counter: {props.counter}</h2>
+        <button className='btn btn-primary' onClick={props.increment}>
+            Increment
     </button>
-    {' '}
-    <button className='btn btn-secondary' onClick={props.doubleAsync}>
-      Double (Async)
+        {' '}
+        <button className='btn btn-secondary' onClick={props.doubleAsync}>
+            Double (Async)
     </button>
-  </div>
+    </div>
 )
 Counter.propTypes = {
-  counter: PropTypes.number.isRequired,
-  increment: PropTypes.func.isRequired,
-  doubleAsync: PropTypes.func.isRequired,
+    counter: PropTypes.number.isRequired,
+    increment: PropTypes.func.isRequired,
+    doubleAsync: PropTypes.func.isRequired,
 }
 
 export default Counter
